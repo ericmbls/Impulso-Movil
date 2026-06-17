5b15ee9fbb73fc0c98915bd41b94e4fea04cf7ef
cffferic <cffferic@gmail.com>
2026-06-17 04:38:12 -0600

    Feat: Perfil 1.0

--- mobile/src/app/features/profile/pages/profile/profile.component.ts
+++ mobile/src/app/features/profile/pages/profile/profile.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonContent, IonIcon } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { 
+  personOutline, 
+  schoolOutline, 
+  mailOutline, 
+  callOutline,
+  calendarOutline,
+  idCardOutline,
+  logOutOutline,
+  chevronForwardOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
   standalone: true,
+  imports: [
+    CommonModule,
+    IonContent,
+    IonIcon
+  ],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent  implements OnInit {
+export class ProfileComponent {
 
-  constructor() { }
+  user = {
+    name: 'Eric Hernández',
+    email: 'eric.hernandez@cbtis61.edu.mx',
+    phone: '744-123-4567',
+    group: '6° A',
+    career: 'Programación',
+    controlNumber: '2023123456',
+    photo: 'E'
+  };
 
-  ngOnInit() {}
+  menuItems = [
+    { icon: 'id-card-outline', label: 'Mi credencial', route: '/credential' },
+    { icon: 'calendar-outline', label: 'Horario', route: '/schedule' },
+    { icon: 'school-outline', label: 'Calificaciones', route: '/grades' },
+    { icon: 'person-outline', label: 'Datos personales', route: '/edit-profile' }
+  ];
 
+  constructor() {
+    addIcons({
+      personOutline,
+      schoolOutline,
+      mailOutline,
+      callOutline,
+      calendarOutline,
+      idCardOutline,
+      logOutOutline,
+      chevronForwardOutline
+    });
+  }
+
+  navigateTo(route: string) {
+    console.log('Navegando a:', route);
+  }
+
+  logout() {
+    console.log('Cerrando sesión...');
+  }
 }
